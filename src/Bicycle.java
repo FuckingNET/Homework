5b01a73d9fb2844d60cfcebc1395c179391a9177
axel <lehander2016@ya.ru>
2023-03-05 23:49:38 +0300

    homework done

--- src/Bicycle.java
+++
@@ -1,5 +0,0 @@
-public class Bicycle extends Vehicle {
-    public Bicycle(String modelName, int wheelsCount) {
-        super(modelName, wheelsCount);
-    }
-}
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,52 @@
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car("car1", 4);
-        Car car2 = new Car("car2", 4);
+        validate("login", "pass", "pass");
+        validate("@fdsgds", "pass", "pass");
+        validate("dfdf", "p&ass", "pass");
+        validate("sdgjakslghjasdlkhfjkdsalhfjsdlabgjalsdjghadsl", "pass", "pass");
+        validate("ddffdf", "pass", "342111");
 
-        Truck truck = new Truck("truck1", 6);
-        Truck truck2 = new Truck("truck2", 8);
+    }
 
-        Bicycle bicycle = new Bicycle("bicycle1", 2);
-        Bicycle bicycle2 = new Bicycle("bicycle2", 2);
+    private static void validate(String login, String password, String confirmPassword) {
+        try {
+            checkLogin(login);
+            checkPassword(password, confirmPassword);
+        } catch (WrongLoginException e) {
+            System.out.println("Invalid login: " + e.getMessage());
+        } catch (WrongPasswordException e) {
+            System.out.println("Invalid password: " + e.getMessage());
+        }
+    }
 
-        ServiceStation station = new ServiceStation();
-        station.check(car);
-        station.check(car2);
-        station.check(bicycle);
-        station.check(bicycle2);
-        station.check(truck);
-        station.check(truck2);
+    private static void checkLogin(String login) {
+        if (hasLengthMoreThan(login) || isNoneAlphanumeric(login)) {
+            throw new WrongLoginException("Login is wrong");
+        }
+    }
+
+    private static void checkPassword(String password, String password2) {
+        if (hasLengthMoreThan(password) || isNoneAlphanumeric(password) ||
+                passwordsNotEquals(password, password2)) {
+            throw new WrongPasswordException("Password is wrong");
+        }
+    }
+
+    private static boolean passwordsNotEquals(String password, String password2) {
+        return !password.equals(password2);
+    }
+
+    private static boolean isNoneAlphanumeric(String str) {
+        final String alphabet = "abcdefghijklmnopqrstuvwxyz1234567890_";
+        for (int i = 0; i < str.length(); i++) {
+            if (!alphabet.contains(String.valueOf(str.charAt(i)))){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean hasLengthMoreThan(String str) {
+        return str.length() > 20;
     }
 }
