5b7bae4e05847a7b7774426528ea1387cf79414e
axel <lehander2016@ya.ru>
2023-03-10 18:27:55 +0300

    Homework done

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,61 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         task1();
         task2();
         task3();
+        task4();
+    }
+
+    private static void task4() {
+        String text = "Расскажите про покупки, " +
+                "Про какие про покупки? " +
+                "Про покупки, про покупки, " +
+                "Про покупочки мои.";
+        text = text.replaceAll("[.?,]", "");
+        List<String> strings = List.of(text.split(" "));
+        Map<String, Integer> countStr = new HashMap<>();
+        for (String s : strings) {
+            if (countStr.get(s) == null) {
+                countStr.put(s, 1);
+            } else {
+                countStr.put(s, countStr.get(s) + 1);
+            }
+        }
+        System.out.println(countStr);
     }
 
     private static void task3() {
         System.out.println("Задача 3");
-        int deliveryDistance = 95;
-        System.out.println(timeDelivery(deliveryDistance));
+        String text = "Расскажите про покупки, " +
+                "Про какие про покупки? " +
+                "Про покупки, про покупки, " +
+                "Про покупочки мои.";
+        text = text.replaceAll("[.?,]", "");
+        Set<String> strings = new HashSet<>(List.of(text.split(" ")));
+        System.out.println(strings);
     }
 
     private static void task2() {
         System.out.println("Задача 2");
-        int clientDeviceYear = 2018;
-        int os = 1;
-        versionApp(os, clientDeviceYear);
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        Set<Integer> numsResult = new HashSet<>();
+        for (Integer num : nums) {
+            if (num % 2 == 0) {
+                numsResult.add(num);
+            }
+        }
+        System.out.println(numsResult);
     }
 
     private static void task1() {
         System.out.println("Задача 1");
-        int year = 2023;
-        isLeapYear(year);
-    }
-
-    private static int timeDelivery(int deliveryDistance) {
-        int deliveryDays = 1;
-        if (deliveryDistance > 20) {
-            deliveryDays++;
-        }
-        if (deliveryDistance > 60) {
-            deliveryDays++;
-        }
-        return deliveryDays;
-    }
-
-    public static void versionApp(int os, int releaseDate) {
-        if (os == 1) {
-            if (releaseDate < 2015) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для iOS по ссылке");
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        for (Integer num : nums) {
+            if (num % 2 != 0) {
+                System.out.println(num);
             }
-        } else {
-            if (releaseDate < 2015) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            } else {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            }
-        }
-    }
-
-    public static void isLeapYear(int year) {
-        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-            System.out.println(year + " год является високосным");
-        } else {
-            System.out.println(year + " год не является високосным");
         }
     }
 }
